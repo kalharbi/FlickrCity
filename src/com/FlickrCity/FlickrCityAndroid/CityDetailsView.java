2c12b44e1aeda0bc871385526a5f87fad76dd369
David Parker <davidwparker@gmail.com>
2012-04-27 11:39:28 -0600

    attempt to get back old files

--- src/com/FlickrCity/FlickrCityAndroid/CityDetailsView.java
+++ src/com/FlickrCity/FlickrCityAndroid/CityDetailsView.java
@@ -1,44 +1,60 @@
 package com.FlickrCity.FlickrCityAndroid;
 
+import java.util.concurrent.ExecutionException;
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+/**
+ * City Details View - Activity to display a city's information and download
+ * photos
+ * 
+ * @author khalid, dparker, hunter
+ */
 public class CityDetailsView extends Activity {
+
+	ConcurrentAPI api;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.citydetails);
 
-//		// setup concurrency
-//		final int numberOfCores = Runtime.getRuntime().availableProcessors();
-//		final double blockingCoefficient = 0.9;
-//		final int poolSize = (int) (numberOfCores / (1 - blockingCoefficient));
-//		TextView cores = (TextView) findViewById(R.id.corestext);
-//		cores.setText(String.valueOf(poolSize));
+		api = new ConcurrentAPI(Constants.FLICKR);
 
 		// get city info
 		Bundle extras = getIntent().getExtras();
 		if (extras != null) {
-			String city = extras.getString("NAME");
-			int lat = extras.getInt("LAT");
-			int lng = extras.getInt("LONG");
+			String city = extras.getString(Constants.NAME);
+			int lat = extras.getInt(Constants.LAT);
+			int lng = extras.getInt(Constants.LNG);
 			TextView citytext = (TextView) findViewById(R.id.textviewcityname);
-			Log.v("CITY", city);
 			TextView lattext = (TextView) findViewById(R.id.textviewlatVal);
-			TextView longtext = (TextView) findViewById(R.id.textviewlongVal);
+			TextView lngtext = (TextView) findViewById(R.id.textviewlngVal);
 			citytext.setText(city);
 			lattext.setText(String.valueOf(lat));
-			longtext.setText(String.valueOf(lng));
+			lngtext.setText(String.valueOf(lng));
 		}
 
 		final Button button = (Button) findViewById(R.id.view_flickr_pictures);
 		button.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				// Perform action on click
+				// TODO call concurrency API
+				TextView cores = (TextView) findViewById(R.id.corestext);
+				cores.setText(String.valueOf(api.getPoolSize()));
+				try {
+					api.call();
+				} catch (InterruptedException e) {
+					// swallow interrupted exception
+					// TODO: handle this
+				} catch (ExecutionException e) {
+					// swallow execution exception
+					// TODO: handle this
+				}
 			}
 		});
 
